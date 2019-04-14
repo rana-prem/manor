ebbef74976cc8b87710311111cb3cad55dd27e38
rana-prem <49530642+rana-prem@users.noreply.github.com>
2019-04-14 23:16:48 +0530

    Animated version

--- js/main.js
+++ js/main.js
@@ -1,17 +1,59 @@
-//trigger the snap scroll
-var snapScroll = $("section").SnapScroll({
-  hashes: true
-});
-console.log(snapScroll);
 
-//Listen for active element change
-//You could listen globally like $(document)
-$("header").on(snapScroll.eventChangeActive, function(evt, newActive) {
-  console.log(evt, newActive);
+
+//scroll trigger
+document.addEventListener('DOMContentLoaded', function(){
+    var trigger = new ScrollTrigger();
+
+
 });
 
-//Listen for visible element change
-//You could listen on a specific element like $("header")
-$(document).on(snapScroll.eventChangeVisible, function(evt, visibleList) {
-  console.log(evt, visibleList.data);
+//Navigation and Hero Animation
+var tl = anime.timeline({
+  easing: 'easeOutQuart',
+  duration: 750,
 });
+
+// Add children
+tl
+.add({
+  targets: '.navbar',
+ translateY: [-50, 0], // from 100 to 250
+ opacity:[0,1],
+ direction: 'alternate',
+ duration:1200,
+ easing:"easeOutSine",
+ delay:300
+})
+.add({
+  targets: '.hero-logo-large',
+  translateY: [50, 0],
+  opacity:[0,1],
+  duration:600,
+  easing:"easeOutQuart",
+  offset: '-=300'
+}, '-=3000')
+.add({
+  targets: '.hero-header',
+  translateY: [50, 0],
+  opacity:[0,1],
+  duration:600,
+  easing:"easeOutQuart",
+  offset: '-=300'
+}, '-=3000')
+.add({
+  targets: '.hero-para',
+  translateY: [50, 0],
+  opacity:[0,1],
+  duration:600,
+  easing:"easeOutQuart",
+  offset: '-=300'
+}, 0 )
+
+.add({
+  targets: '.btn-standard',
+  translateY: [50, 0],
+  opacity:[0,1],
+  duration:600,
+  easing:"easeOutQuart",
+  offset: '-=300'
+}, 0 );
